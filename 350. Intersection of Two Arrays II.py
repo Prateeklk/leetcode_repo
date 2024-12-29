b42719f383b47f8d0a6163ea931a00124e753112
Prateek Lokesh Kumbar <68516313+Prateeklk@users.noreply.github.com>
2024-12-28 22:39:38 -0500

    Add files via upload

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        counter1 = Counter(nums1)
+        counter2 = Counter(nums2)
+
+        result = []
+
+        for num in counter1:
+            if num in counter2:  
+                min_count = min(counter1[num], counter2[num])
+                result.extend([num] * min_count)
+
+        return result
+
+
